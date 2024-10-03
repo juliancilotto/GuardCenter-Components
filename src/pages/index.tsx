4ec6ff3152cdb02be8ececf9846ad6724f29cc5d
Júlia Ancilotto <juliancilotto@gmail.com>
2024-10-03 12:13:50 -0300

    fix: engine

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -35,14 +35,12 @@ import {
   ArrowUpOutlined,
   CheckOutlined,
   CloseOutlined,
-  InfoOutlined,
   PlusOutlined,
   UserOutlined,
 } from "@ant-design/icons";
 import { AlertTag } from "@/components/Tags/AlertTag";
 import { HelpCircle } from "@/assets/icons/HelpCircle";
 import ptBR from "antd/es/date-picker/locale/pt_BR";
-import ptBRGlobal from "antd/es/locale/pt_BR";
 import { Search } from "@/assets/icons/Search";
 import TeamMembersTable from "@/components/Tables/teamMembers";
 import CustomersTable from "@/components/Tables/customers";
